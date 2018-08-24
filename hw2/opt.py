791643e329ede860b3c260be9c0d59cda682bf7c
Kaifeng Gu <kgu@Kaifengs-MacBook-Pro.local>
2018-08-23 23:10:35 -0400

    modify opt.py

--- hw2/opt.py
+++ hw2/opt.py
@@ -3,13 +3,17 @@ import numpy as np
 
 if __name__ == '__main__':
     N = 1000
-    x0_matrix = (np.random.rand(N,3)*2)
+    x0_matrix = (np.random.rand(N,3)*2000-1000)
     xa = []
+    fa = []
     for i in range(N):
         x0 = x0_matrix[i]
         re = minimize(rosen, x0, method='BFGS')
         xa.append(re.x)
+        fa.append(re.fun)
     xa = np.array(xa)
-    xaT = xa.T
-    xa_average = [np.average(xaT[0]),np.average(xaT[1]),np.average(xaT[2])]
-    print(xa_average)
+    opt_value = np.min(fa)
+    print("optimal value so far:",opt_value)
+    for i in range(N):
+        if fa[i]==opt_value:
+            print("corresponding x values:", xa[i])
